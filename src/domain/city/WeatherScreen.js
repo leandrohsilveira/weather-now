924c38a824e96d6366fa8b008c16a593391d5987
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-12 00:31:52 -0300

    Connect WeatherScreen component to redux

--- src/domain/city/WeatherScreen.js
+++ src/domain/city/WeatherScreen.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 import CardGrid from './../../components/card/CardGrid';
 import CityCard from './CityCard';
+import { restoreWeather } from './weather.actions';
+
+const mapStateToProps = state => ({
+    cities: state.weather
+});
+
+const mapDispatchToProps = dispatch => ({
+    restore: () => dispatch(restoreWeather())
+});
 
 const WeatherScreen = (Comp => {
-
+    Comp = connect(mapStateToProps, mapDispatchToProps)(Comp);
     return Comp;
 })(
     class _WeatherScreen extends Component {
