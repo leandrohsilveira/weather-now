ce1ccd7afca152f34c2f4aee3084ee40b3f02efa
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-11 21:32:30 -0300

    Weather color

--- src/domain/city/CityCardContent.js
+++ src/domain/city/CityCardContent.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './CityCardContent.css';
 
 const CityCardContent = ({value, unity = '°'}) => {
-
+    let weather = 'normal';
+    if(value > 25) {
+        weather = 'hot';
+    } else if(value <= 5) {
+        weather = 'cold';
+    }
     return (
         <div className="CityCardContent-container flex">
-            <span className="CityCardContent-value flex">{value}</span>
-            <span className="CityCardContent-unity flex">{unity}</span>
+            <span className={`CityCardContent-value CityCardContent-${weather} flex`}>{value}</span>
+            <span className={`CityCardContent-unity CityCardContent-${weather} flex`}>{unity}</span>
         </div>
     );
 };
