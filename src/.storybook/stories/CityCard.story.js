0e6a6a250b72450b96c306d9197f2474c6bf056a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-12 19:40:40 -0300

    Main CityCard stories

--- src/.storybook/stories/CityCard.story.js
+++ src/.storybook/stories/CityCard.story.js
@@ -21,3 +21,18 @@ storiesOf('CityCard', module)
     .add('when something went wrong', () => (
         <CityCard title="City, CC" error={true} onTryAgain={action('Trying again...')} />
     ))
+    .add('when the main card weather is normal', () => (
+        <CityCard type="main" title="City, CC" value={20} humidity={70} pressure={890} />
+    ))
+    .add('when the main card weather is hot', () => (
+        <CityCard type="main" title="City, CC" value={30} humidity={67} pressure={837} />
+    ))
+    .add('when the main card weather is cold', () => (
+        <CityCard type="main" title="City, CC" value={3} humidity={84} pressure={955} />
+    ))
+    .add('when the main card weather value is loading', () => (
+        <CityCard type="main" title="City, CC" loading={true} />
+    ))
+    .add('when something went wrong with the main card', () => (
+        <CityCard type="main" title="City, CC" error={true} onTryAgain={action('Trying again...')} />
+    ))
