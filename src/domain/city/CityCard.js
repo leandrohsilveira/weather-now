297320fd3ff54d90f6c22828f02be91b0fadb25f
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-12 00:32:39 -0300

    Pass umidity and pressure props to CityCardFooter component

--- src/domain/city/CityCard.js
+++ src/domain/city/CityCard.js
@@ -8,13 +8,13 @@ import CityCardError from './CityCardError';
 
 import './CityCard.css';
 
-const CityCard = ({ loading, title, value, unity, type = 'alt', error, onTryAgain }) => (
+const CityCard = ({ loading, title, value, umidity, pressure, unity, type = 'alt', error, onTryAgain }) => (
     <Card title={title} className={`CityCard-card CityCard-${type}`}>
         <div className={`CityCard-container flex`}>
             <Loading loading={loading}>
                 <CityCardError error={error} onTryAgain={onTryAgain}>
                     <CityCardContent value={value} unity={unity} />
-                    <CityCardFooter type={type} />
+                    <CityCardFooter type={type} umidity={umidity} pressure={pressure} />
                 </CityCardError>
             </Loading>
         </div>
