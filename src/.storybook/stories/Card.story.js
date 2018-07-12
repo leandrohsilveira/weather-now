2d87655082f15e0cdf898513ea58c112001ed9ff
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-12 19:40:18 -0300

    Added card stories

---
+++ src/.storybook/stories/Card.story.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import '../../index.css';
+import Card from '../../components/card/Card';
+
+storiesOf('Card', module)
+    .add('when is completely empty', () => (
+        <Card />
+    ))
+    .add('when has a title', () => (
+        <Card title="City, CC" />
+    ))
+    .add('when has a content', () => (
+        <Card>
+            <div>The card's content</div>
+        </Card>
+    ))
+    .add('when has both tile and content', () => (
+        <Card title="City, CC">
+            <div>The card's content</div>
+        </Card>
+    ))
