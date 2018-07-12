5ab34fb253ed3e0dfb3945b7c62dffea13524fda
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-11 22:47:32 -0300

    Display loading state and error state

--- src/domain/city/CityCard.js
+++ src/domain/city/CityCard.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Card from './../../components/card/Card';
+import Loading from './../../components/loading/Loading';
 
 import CityCardContent from './CityCardContent';
 import CityCardFooter from './CityCardFooter';
+import CityCardError from './CityCardError';
 
 import './CityCard.css';
 
-const CityCard = ({title, value, unity, type = 'alt'}) => (
+const CityCard = ({ loading, title, value, unity, type = 'alt', error, onTryAgain }) => (
     <Card title={title} className={`CityCard-card CityCard-${type}`}>
         <div className={`CityCard-container flex`}>
-            <CityCardContent value={value} unity={unity} />
-            <CityCardFooter type={type} />
+            <Loading loading={loading}>
+                <CityCardError error={error} onTryAgain={onTryAgain}>
+                    <CityCardContent value={value} unity={unity} />
+                    <CityCardFooter type={type} />
+                </CityCardError>
+            </Loading>
         </div>
     </Card>
 );
