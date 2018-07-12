abe493ba1c65d9723879b1479ac0539318b19547
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-11 21:53:57 -0300

    React Storybook

---
+++ src/.storybook/stories/CityCard.story.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import '../../index.css';
+import CityCard from '../../domain/city/CityCard';
+
+storiesOf('CityCard', module)
+    .add('when the weather is normal', () => (
+        <CityCard title="City, CC" value={20} />
+    ))
+    .add('when the weather is hot', () => (
+        <CityCard title="City, CC" value={30} />
+    ))
+    .add('when the weather is cold', () => (
+        <CityCard title="City, CC" value={3} />
+    ))
+    .add('when the weather value is loading', () => (
+        <CityCard title="City, CC" value={null} />
+    ))
