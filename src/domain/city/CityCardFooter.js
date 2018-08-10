047b7f8113b8313557449e6925c657ed2c259016
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-10 00:22:47 -0300

    Using styled-components on CityCardFooter

--- src/domain/city/CityCardFooter.js
+++ src/domain/city/CityCardFooter.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import moment from 'moment';
+import styled, { css } from 'styled-components';
 
 import Grid from './../../components/grid/Grid';
 import CityCardPropDisplay from './CityCardPropDisplay';
-import './CityCardFooter.css';
+
+const Container = styled.div`
+    flex-direction: column;
+    background-color: rgba(248, 248, 248, 1);
+    border-radius: 0 0 4px 4px;
+    min-height: ${({ type }) => type === 'main' ? 85 : 40}px;
+`
+const Updated = styled.span`
+    font-size: 0.65em;
+    color: #b4b4b4;
+    align-self: center;
+
+    ${({type}) => type === 'main' && css`
+        align-self: start;
+    `}
+
+`
 
 const CityCardFooter = ({ type = 'alt', date, humidity, pressure }) => (
-    <div className={`CityCardFooter-container CityCardFooter-${type} flex`}>
+    <Container className="flex" type={type}>
         {type === 'main' && (
-            <Grid className="CityCardFooter-row-top" direction="row" justifyContent="space-around">
+            <Grid flex={6} direction="row" justifyContent="space-around">
                 <CityCardPropDisplay label="Humidity" value={humidity} unity="%" />
                 <CityCardPropDisplay label="Pressure" value={pressure} unity="hPa" />
             </Grid>
         )}
-        <Grid className="CityCardFooter-row-bottom" direction="row" justifyContent="center">
-            <span className={`CityCardFooter-updated ${type}`}>Updated at {moment(date).format(('LTS'))}</span>
+        <Grid flex={4} direction="row" justifyContent="center">
+            <Updated type={type}>Updated at {moment(date).format(('LTS'))}</Updated>
         </Grid>
-    </div>
+    </Container>
 );
 
 export default CityCardFooter;
