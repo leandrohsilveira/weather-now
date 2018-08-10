237a6b4abc70a46eb8238df767513910a695cae6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-10 00:23:01 -0300

    Using styled-components on CityCardPropDisplay

--- src/domain/city/CityCardPropDisplay.js
+++ src/domain/city/CityCardPropDisplay.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import numeral from 'numeral';
+import styled from 'styled-components';
 
 import Grid from './../../components/grid/Grid';
 
-import './CityCardPropDisplay.css';
+const Label = styled(Grid)`
+    margin-top: 5px;
+    text-transform: uppercase;
+    font-size: 0.72em;
+    color: #b4b4b4;
+    text-align: center;
+`
+
+const Value = styled.span`
+    font-size: 1.1em;
+    margin-top: 5px;
+    color: #737C84;
+    align-self: baseline;
+`
+
+const Unit = styled.span`
+    margin-top: 3px;
+    font-size: 0.75em;
+    color: #737C84;
+    align-self: baseline;
+`
 
 const CityCardPropDisplay = ({label, value, unity}) => (
     <Grid direction="column" justifyContent="center" alignSelf="center">
-        <Grid className="CityCardPropDisplay-label" direction="row" justifyContent="center">
-            {label}
-        </Grid>
-        <Grid className="CityCardPropDisplay-description" direction="row" justifyContent="center">
-            <span className="CityCardPropDisplay-value">{numeral(value).format('0')}</span>
-            <span className="CityCardPropDisplay-unity">{unity}</span>
+        <Label direction="row" justifyContent="center">{label}</Label>
+        <Grid direction="row" justifyContent="center">
+            <Value>{numeral(value).format('0')}</Value>
+            <Unit>{unity}</Unit>
         </Grid>
     </Grid>
 );
