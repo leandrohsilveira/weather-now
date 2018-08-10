4b4d98cf391706cec8f19affed92831effd651d0
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-09 23:08:26 -0300

    Use styled-components on Layout component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,16 +1,46 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
 import TopBar from './TopBar';
 
-import './Layout.css';
+const Main = styled.main`
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+`
+
+const Container = styled.div`
+    flex-direction: column;
+    justify-content: center;
+    background-color: #f1f1f1;
+    margin-top: 55px;
+    min-height: 835px;
+    width: 1025px;
+    -webkit-box-shadow: inset 0px 1px 1px 0px rgba(51,51,51,0.1);
+    -moz-box-shadow: inset 0px 1px 1px 0px rgba(51,51,51,0.1);
+    box-shadow: inset 0px 1px 1px 0px rgba(51,51,51,0.1);
+
+    @media screen and (max-width: 1025px) {
+        -webkit-box-shadow: none;
+        -moz-box-shadow: none;
+        box-shadow: none;
+        width: 100%;
+    }
+
+    @media screen and (max-width: 599px) {
+        min-height: auto;
+        justify-content: start;
+    }
+`
 
 const Layout = ({ children }) => (
     <Fragment>
         <TopBar />
-        <main className="Layout-main flex">
-            <div className="Layout-container flex">
+        <Main className="flex">
+            <Container className="flex">
                 {children}
-            </div>
-        </main>
+            </Container>
+        </Main>
     </Fragment>
 );
 
