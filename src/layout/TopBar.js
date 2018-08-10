e5ce408e15430665527478a774c0a8c55143722b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-09 23:08:14 -0300

    Use styled-components on TopBar component

--- src/layout/TopBar.js
+++ src/layout/TopBar.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import defaultLogo from './logo.svg';
-import './TopBar.css';
 
-const TopBar = ({logo = defaultLogo}) => (
-    <header className="TopBar-header">
-        <div className="TopBar-content flex">
-            <img className="TopBar-logo" src={logo} alt="WeatherNow Logo" />
-        </div>
-    </header>
+const Header = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Content = styled.div`
+    width: 100%;
+    height: 55px;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #ffffff;
+    
+    @media screen and (max-width: 1025px) {
+        -webkit-box-shadow: 0px 1px 1px 0px rgba(51,51,51,0.1);
+        -moz-box-shadow: 0px 1px 1px 0px rgba(51,51,51,0.1);
+        box-shadow: 0px 1px 1px 0px rgba(51,51,51,0.1);
+    }
+`
+
+const Logo = styled.img`
+    align-self: center;
+    height: 24px;
+`
+
+const TopBar = ({ logo = defaultLogo }) => (
+    <Header>
+        <Content className="flex">
+            <Logo src={logo} alt="WeatherNow Logo" />
+        </Content>
+    </Header>
 );
 
 export default TopBar;
