0394d62f87a071c5e3e0084cba023ffed714717e
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-09 22:55:51 -0300

    TopBar logo prop

--- src/layout/TopBar.js
+++ src/layout/TopBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import logo from './logo.svg';
+import defaultLogo from './logo.svg';
 import './TopBar.css';
 
-const TopBar = (props) => (
+const TopBar = ({logo = defaultLogo}) => (
     <header className="TopBar-header">
         <div className="TopBar-content flex">
             <img className="TopBar-logo" src={logo} alt="WeatherNow Logo" />
