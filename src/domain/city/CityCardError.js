e4174d5a2b09194f7d6196b441077087f6ed059d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-08-09 23:53:51 -0300

    Use styled-components on CityCardError

--- src/domain/city/CityCardError.js
+++ src/domain/city/CityCardError.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Grid from './../../components/grid/Grid';
 
-import './CityCardError.css';
+const Message = styled.div`
+    color: #ed1946;
+    align-self: center;
+`
+
+const Button = styled.button`
+    cursor: pointer;
+    color: #737c84;
+    background-color: transparent;
+
+    font-size: 1.05em;
+
+    margin-top: 20px;
+    line-height: 38px;
+    width: 100px;
+
+    border: 2px solid #737c84;
+    border-radius: 30px;
+
+    align-self: center;
+    justify-content: center;
+    align-content: center;
+
+    :active,
+    :focus {
+        outline: none;
+    }
+
+    :hover {
+        outline: none;
+        background-color: #f1f1f1; 
+    }
+
+`
 
 const CityCardError = ({error, onTryAgain, children}) => {
     if(error) {
         return (
             <Grid direction="column" justifyContent="center">
-                <div className="CityCardError-message flex">{error.message || 'Something went wrong'}</div>
+                <Message className="flex">{error.message || 'Something went wrong'}</Message>
                 {typeof onTryAgain === 'function' && (
-                    <button type="button" className="CityCardError-button flex" onClick={onTryAgain}>Try Again</button>
+                    <Button type="button" className="flex" onClick={onTryAgain}>Try Again</Button>
                 )}
             </Grid>
         )
